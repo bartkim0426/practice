32636612a34b74bb32ac1f2b076cb521c0383944
bartkim0426 <seulchankim@Seului-MacBook-Pro.local>
2017-04-06 21:55:33 +0900

    Add next url parameter function

--- practice/practice/views/auth/login.py
+++ practice/practice/views/auth/login.py
@@ -5,20 +5,18 @@ from django.contrib.auth import authenticate, login as auth_login
 
 
 def login(request):
+    
     if (request.method == "POST"):
         username = request.POST.get("username")
         password = request.POST.get("password")
+        next_page = request.POST.get("next_page") or "home"
 
         user = User.objects.get(username=username)
 
         if user.is_authenticated():
             auth_login(request, user)
 
-            return redirect(
-                    reverse(
-                        "posts:list",
-                        )
-                    )
+            return redirect(next_page)
 
     return render(
             request,
